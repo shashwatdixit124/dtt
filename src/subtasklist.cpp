d2f1934d87fc582354df1b7f2818142444f61e18
Shashwat Dixit <shashwatdixit124@gmail.com>
2017-07-06 21:26:02 +0530

    subtask list used in gui

---
+++ src/subtasklist.cpp
@@ -0,0 +1,112 @@
+/*
+ *   This file is part of dtt, a Daily Task Tracker
+ *   Copyright 2017 Shashwat Dixit <shashwatdixit124@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "subtasklist.h"
+#include "subtask.h"
+
+#include <QObject>
+#include <QAbstractListModel>
+
+SubTaskList::SubTaskList(QObject *parent) : QObject(parent)
+{
+}
+
+SubTaskList::~SubTaskList()
+{
+}
+
+QVariant SubTaskList::data(const QModelIndex& index, int role) const
+{
+	if (index.row() < 0 || index.row() >= m_subTasks.count())
+		return QVariant();
+	SubTask t = m_subTasks[index.row()];
+	if(role == ID)
+		return t.id();
+	else if(role == DESCRIPTION)
+		return t.description();
+	else if(role == CREATEDON)
+		return t.createdOn().toString("MMMM dd , yyyy");
+	else if(role == UPDATEDON)
+		return t.updatedOn().toString("MMMM dd , yyyy");
+	else if(role == STATUS)
+		return t.status();
+	return QVariant();
+}
+
+int SubTaskList::rowCount(const QModelIndex& parent) const
+{
+	Q_UNUSED(parent);
+	return m_tasks.count();
+}
+
+void SubTaskList::add(SubTask st)
+{
+	if(st.status() != SubTask::PENDING)
+		return;
+	beginInsertRows(QModelIndex(), rowcount() , rowCount());
+	m_subTasks.push_front(st);
+	endInsertRows();
+}
+
+void SubTaskList::update(SubTask st)
+{
+	if(st.status() != SubTask::PENDING)
+		return;
+
+	for(int i = 0;i<rowCount();i++)
+	{
+		SubTask temp = m_subTasks[i];
+		if(temp.id() == st.id()) {
+			m_subTasks[i].setStatus(SubTask::COMPLETED);
+			beginRemoveRows(QModelIndex(), i , i);
+			endRemoveRows();
+			beginInsertRows(QModelIndex(), i , i);
+			endInsertRows();
+			break;
+		}
+	}
+}
+
+void SubTaskList::remove(SubTask st)
+{
+	if(st.status() != SubTask::INVALID)
+		return;
+	for(int i = 0;i<rowCount();i++)
+	{
+		SubTask temp = m_subTasks[i];
+		if(temp.id() == st.id()) {
+			m_subTasks.removeAt(i);
+			beginRemoveRows(QModelIndex(), i , i);
+			endRemoveRows();
+			break;
+		}
+	}
+}
+
+QHash<int, QByteArray> SubTaskList::roleNames() const
+{
+	QHash<int, QByteArray> roles;
+	roles[ID] = "_ST_id";
+	roles[DESCRIPTION] = "_ST_description";
+	roles[CREATEDON] = "_ST_createdon";
+	roles[UPDATEDON] = "_ST_updatedon";
+	roles[STATUS] = "_ST_status";
+	return roles;
+}
---
+++ src/subtasklist.h
@@ -0,0 +1,57 @@
+/*
+ *   This file is part of dtt, a Daily Task Tracker
+ *   Copyright 2017 Shashwat Dixit <shashwatdixit124@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SUBTASKLIST_H
+#define SUBTASKLIST_H
+
+#include "subtask.h"
+
+#include <QObject>
+#include <QAbstractListModel>
+
+class SubTaskList : public QAbstractListModel
+{
+	Q_OBJECT
+public:
+
+	enum SubTaskInfo {
+		ID  = Qt::UserRole + 1,
+		DESCRIPTION,
+		CREATEDON,
+		UPDATEDON,
+		STATUS
+	};
+
+	explicit SubTaskList(QObject *parent = nullptr);
+	~SubTaskList();
+	int rowCount(const QModelIndex & parent = QModelIndex()) const;
+	QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+
+	void add(SubTask);
+	void update(SubTask);
+	void remove(SubTask);
+
+protected:
+	QHash<int, QByteArray> roleNames() const;
+	QList<SubTask> m_subTasks;
+
+};
+
+#endif
